7900f927791299d8688cbfeef7f231c28dafabf1
Rajabboy <rajrajrajradju@gmail.com>
2022-11-09 14:06:31 +0500

    corrected for PEP

--- getmetadata/views.py
+++ getmetadata/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import os
@@ -8,10 +7,9 @@ import os
 def getmeta(request):
     data = []
     for i in os.listdir():
-        d = {}
-        d["name"] = i
-        d["type"] = 'folder' if os.path.isdir(os.path.abspath(i)) else 'file'
-        d["time"] = round(os.path.getctime(i))
+        d = {"name": i,
+             "type": 'folder' if os.path.isdir(os.path.abspath(i)) else 'file',
+             "time": round(os.path.getctime(i))}
         print(d["name"], d["type"], d["time"])
         data.append(d)
 
